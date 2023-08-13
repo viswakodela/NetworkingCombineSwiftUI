c29007b3418b35a4308e7515e0298ee73394df29
Viswa Kodela <viswajith.kodela@td.com>
2023-08-13 15:09:04 -0400

    Initial Commit

---
+++ NetworkingCombineSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NetworkingCombineSwiftUI
+//
+//  Created by kodelv3 on 2023-08-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkingCombineSwiftUI/NetworkingCombineSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingCombineSwiftUIApp.swift
+//  NetworkingCombineSwiftUI
+//
+//  Created by kodelv3 on 2023-08-13.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingCombineSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
